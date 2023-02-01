9975eb29f5e3a120449a14b655f87a32ea67222b
Akash Raj <akashraj0409@gmail.com>
2023-02-01 23:10:44 +0530

    added filter code

--- controllers/products.js
+++ controllers/products.js
@@ -2,7 +2,20 @@
 const Product = require("../models/product")
 
 const getAllProducts = async(req,res)=>{
-    const myData = await Product.find({});
+
+    const {company , name } = req.query;
+    const queryObject = {};
+
+    if(company){
+        queryObject.company = { $regex : company , $options : "i"};
+    }
+
+    if(name){
+        queryObject.name = { $regex : name , $options : "i"};
+    }
+
+
+    const myData = await Product.find(queryObject);
     res.status(200).json({myData});
 };
 
