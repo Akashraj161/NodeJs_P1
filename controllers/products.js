5912be324014b868618459df190a01f4fcd8f6b0
Akash Raj <akashraj0409@gmail.com>
2023-02-08 10:47:19 +0530

    Search,sort,select and pagination added

--- controllers/products.js
+++ controllers/products.js
@@ -6,6 +6,8 @@ const getAllProducts = async(req,res)=>{
     const {company , name , sort , select } = req.query;
     const queryObject = {};
 
+    // Filter the document on basic of name and company.
+
     if(company){
         queryObject.company = { $regex : company , $options : "i"};
     }
@@ -15,6 +17,8 @@ const getAllProducts = async(req,res)=>{
     }
 
     let apiData = Product.find(queryObject);
+
+    //Sort Functionality
      
     if(sort){
         //let sortUrlFix = sort.replace(","," ");
@@ -22,6 +26,9 @@ const getAllProducts = async(req,res)=>{
         // console.log(sortUrlFix);
         apiData = apiData.sort(sortUrlFix);
     }
+
+    // Return specific document field using SELECT
+
     if(select){
         //let sortUrlFix = sort.replace(","," ");
         let selectUrlFix = select.split(",").join(" ");
@@ -29,8 +36,17 @@ const getAllProducts = async(req,res)=>{
         apiData = apiData.select(selectUrlFix);
     }
 
+    // Pagination code
+
+    let page = Number(req.query.page)|| 1;
+    let limit = Number(req.query.limit) || 3;
+
+    let skip = (page - 1)*limit;
+
+    apiData = apiData.skip(skip).limit(limit);
+
     const myData = await apiData;
-    res.status(200).json({myData});
+    res.status(200).json({myData , nbHits:myData.length});
 };
 
 const getAllProductTesting = async(req,res)=>{
